8cf94691e5e695618ba7116ada4c41ce3777c4a3
Free <xufree888@gmail.com>
2024-01-30 14:08:22 +0800

    fix: 提交网签错误
    1. 提交网签数据将数组中的null元素替换为空字符串

--- src/Core/Main.php
+++ src/Core/Main.php
@@ -25,7 +25,7 @@ class Main extends BasicSHSign {
      * Author: DQ
      */
     public function getAccessToken() {
-        $url  = $this->getUrl('/oauth/v2/token');
+        $url = $this->getUrl('/oauth/v2/token');
         $data = $this->config->merge(['grant_type' => 'password', 'scope' => 'read']);
 
         return $this->httpPostJson($url, $data);
@@ -64,12 +64,12 @@ class Main extends BasicSHSign {
      * Author: DQ
      */
     public function getTransactionInfo($cardNo = '') {
-        $url     = $this->getUrl('/business/commonhouse/openapi/getTransactionInfo');
-        $data    = [
+        $url = $this->getUrl('/business/commonhouse/openapi/getTransactionInfo');
+        $data = [
             'access_token' => $this->getToken(),
-            'companyCode'  => $this->config['companyCode'],
-            'companyName'  => $this->config['companyName'],
-            'cardNo'       => $cardNo
+            'companyCode' => $this->config['companyCode'],
+            'companyName' => $this->config['companyName'],
+            'cardNo' => $cardNo
         ];
         $headers = [
             'Content-Type' => 'application/x-www-form-urlencoded;charset=utf-8',
@@ -81,19 +81,19 @@ class Main extends BasicSHSign {
     /**
      * 运营公司统一从网签平台获取合同号
      *
-     * @param string $pId               项目ID
-     * @param string $projectName       项目名称
+     * @param string $pId 项目ID
+     * @param string $projectName 项目名称
      * @param string $contractAttribute 合同属性：个人：01，单位：02
      *                                  Author: DQ
      */
     public function getContractSeqNo($pId = '', $projectName = '', $contractAttribute = '01') {
-        $url  = $this->getUrl('/business/commonhouse/openapi/getContractSeqNo');
+        $url = $this->getUrl('/business/commonhouse/openapi/getContractSeqNo');
         $data = [
-            'access_token'      => $this->getToken(),
-            'companyId'         => $this->config['companyId'],
-            'companyName'       => $this->config['companyName'],
-            'PID'               => $pId,
-            'projectName'       => $projectName,
+            'access_token' => $this->getToken(),
+            'companyId' => $this->config['companyId'],
+            'companyName' => $this->config['companyName'],
+            'PID' => $pId,
+            'projectName' => $projectName,
             'contractAttribute' => $contractAttribute,
         ];
 
@@ -109,11 +109,11 @@ class Main extends BasicSHSign {
      *
      * @param string $pId
      * @param string $projectName
-     * @param string $HID                 房源ID
-     * @param array  $contractInfo        合同信息
-     * @param array  $uploadTenantBean    承租人信息
-     * @param array  $uploadHouseInfoBean 房屋信息
-     * @param array  $togetherList        同住人信息
+     * @param string $HID 房源ID
+     * @param array $contractInfo 合同信息
+     * @param array $uploadTenantBean 承租人信息
+     * @param array $uploadHouseInfoBean 房屋信息
+     * @param array $togetherList 同住人信息
      *
      * @return mixed
      * @throws \ErrorException
@@ -123,52 +123,53 @@ class Main extends BasicSHSign {
     public function uploadContract($pId = '', $projectName = '', $HID = '', $contractInfo = [], $uploadTenantBean = [], $uploadHouseInfoBean = [], $togetherList = []) {
         $url = $this->getUrl('/business/commonhouse/openapi/uploadContract');
 
-        $emptyTenantBean  = [
-            "transactionNo"    => "",
-            "name"             => "",
-            "sex"              => "",
-            "nation"           => "",
-            "birthday"         => "",
-            "address"          => "",
-            "cardNo"           => "",
-            "age"              => "",
-            "registeredRight"  => "",
-            "registeredType"   => "",
-            "mobile"           => "",
-            "telephone"        => "",
-            "email"            => "",
-            "education"        => "",
-            "livingCardNo"     => "",
-            "livingCardTime"   => "",
-            "bankAccount"      => "",
-            "bank"             => "",
-            "bankProxy"        => "",
-            "companyName"      => "",
-            "contact"          => "",
-            "taxpayerAddress"  => "",
-            "taxpayerMobile"   => "",
+        $emptyTenantBean = [
+            "transactionNo" => "",
+            "name" => "",
+            "sex" => "",
+            "nation" => "",
+            "birthday" => "",
+            "address" => "",
+            "cardNo" => "",
+            "age" => "",
+            "registeredRight" => "",
+            "registeredType" => "",
+            "mobile" => "",
+            "telephone" => "",
+            "email" => "",
+            "education" => "",
+            "livingCardNo" => "",
+            "livingCardTime" => "",
+            "bankAccount" => "",
+            "bank" => "",
+            "bankProxy" => "",
+            "companyName" => "",
+            "contact" => "",
+            "taxpayerAddress" => "",
+            "taxpayerMobile" => "",
             "companyPartition" => "",
-            "workPartition"    => "",
-            "zip"              => "",
-            "remark"           => "",
-            "livingStartTime"  => "",
-            "livingStatus"     => ""
+            "workPartition" => "",
+            "zip" => "",
+            "remark" => "",
+            "livingStartTime" => "",
+            "livingStatus" => ""
         ];
         $uploadTenantBean = array_merge($emptyTenantBean, $uploadTenantBean);
 
         $data = [
-            'companyId'           => $this->config['companyId'],
-            'companyName'         => $this->config['companyName'],
-            'PID'                 => $pId,
-            'HID'                 => $HID,
-            'projectName'         => $projectName,
-            'uploadTenantBean'    => $uploadTenantBean,
+            'companyId' => $this->config['companyId'],
+            'companyName' => $this->config['companyName'],
+            'PID' => $pId,
+            'HID' => $HID,
+            'projectName' => $projectName,
+            'uploadTenantBean' => $uploadTenantBean,
             'uploadHouseInfoBean' => $uploadHouseInfoBean,
         ];
         if ($togetherList) {
             $data['togetherList'] = $togetherList;
         }
-        $data    = array_merge($data, $contractInfo);
+        $data = array_merge($data, $contractInfo);
+        $this->convertNullToEmptyString($data);
         $headers = [
             'Content-Type' => 'application/json',
             'access_token' => $this->getToken(),
@@ -180,8 +181,8 @@ class Main extends BasicSHSign {
     /**
      * 退租申请
      *
-     * @param string $pId           项目ID
-     * @param string $projectName   项目名称
+     * @param string $pId 项目ID
+     * @param string $projectName 项目名称
      * @param        $HID           房源ID
      * @param        $contractNO    合同编号
      *
@@ -190,15 +191,15 @@ class Main extends BasicSHSign {
      * Author: DQ
      */
     public function returnContract($pId = '', $projectName = '', $HID, $contractNO) {
-        $url     = $this->getUrl('/business/commonhouse/openapi/returnContrart');
-        $data    = [
+        $url = $this->getUrl('/business/commonhouse/openapi/returnContrart');
+        $data = [
             'access_token' => $this->getToken(),
-            'companyId'    => $this->config['companyId'],
-            'companyName'  => $this->config['companyName'],
-            'PID'          => $pId,
-            'projectName'  => $projectName,
-            'HID'          => $HID,
-            'contractNO'   => $contractNO
+            'companyId' => $this->config['companyId'],
+            'companyName' => $this->config['companyName'],
+            'PID' => $pId,
+            'projectName' => $projectName,
+            'HID' => $HID,
+            'contractNO' => $contractNO
         ];
         $headers = [
             'Content-Type' => 'application/x-www-form-urlencoded;charset=utf-8',
@@ -210,11 +211,11 @@ class Main extends BasicSHSign {
     /**
      * 退租确认（改变合同信息和改变房源信息）
      *
-     * @param string $pId                 项目ID
-     * @param string $projectName         项目名称
-     * @param string $HID                 房源ID
-     * @param string $contractNO          合同编号
-     * @param array  $returnhouseInfoBean 房源信息
+     * @param string $pId 项目ID
+     * @param string $projectName 项目名称
+     * @param string $HID 房源ID
+     * @param string $contractNO 合同编号
+     * @param array $returnhouseInfoBean 房源信息
      *
      * @return mixed
      * @throws \ErrorException
@@ -222,14 +223,14 @@ class Main extends BasicSHSign {
      * Author: DQ
      */
     public function confirmReturnContrart($pId = '', $projectName = '', $HID = '', $contractNO = '', $returnhouseInfoBean = []) {
-        $url     = $this->getUrl('/business/commonhouse/openapi/confirmReturnContrart');
-        $data    = [
-            'companyId'           => $this->config['companyId'],
-            'companyName'         => $this->config['companyName'],
-            'PID'                 => $pId,
-            'projectName'         => $projectName,
-            'HID'                 => $HID,
-            'contractNO'          => $contractNO,
+        $url = $this->getUrl('/business/commonhouse/openapi/confirmReturnContrart');
+        $data = [
+            'companyId' => $this->config['companyId'],
+            'companyName' => $this->config['companyName'],
+            'PID' => $pId,
+            'projectName' => $projectName,
+            'HID' => $HID,
+            'contractNO' => $contractNO,
             'returnhouseInfoBean' => $returnhouseInfoBean,
         ];
         $headers = [
@@ -243,10 +244,10 @@ class Main extends BasicSHSign {
     /**
      * 续租确认并改变房源状态
      *
-     * @param string $pId         项目ID
+     * @param string $pId 项目ID
      * @param string $projectName 项目名称
-     * @param string $HID         房源ID
-     * @param string $contractNO  合同编号
+     * @param string $HID 房源ID
+     * @param string $contractNO 合同编号
      *
      * @return mixed
      * @throws \ErrorException
@@ -254,15 +255,15 @@ class Main extends BasicSHSign {
      * Author: DQ
      */
     public function confirmResignContract($pId = '', $projectName = '', $HID = '', $contractNO = '') {
-        $url     = $this->getUrl('/business/commonhouse/openapi/confirmResignContract');
-        $data    = [
+        $url = $this->getUrl('/business/commonhouse/openapi/confirmResignContract');
+        $data = [
             'access_token' => $this->getToken(),
-            'companyId'    => $this->config['companyId'],
-            'companyName'  => $this->config['companyName'],
-            'PID'          => $pId,
-            'projectName'  => $projectName,
-            'HID'          => $HID,
-            'contractNO'   => $contractNO
+            'companyId' => $this->config['companyId'],
+            'companyName' => $this->config['companyName'],
+            'PID' => $pId,
+            'projectName' => $projectName,
+            'HID' => $HID,
+            'contractNO' => $contractNO
         ];
         $headers = [
             'Content-Type' => 'application/x-www-form-urlencoded;charset=utf-8',
@@ -276,12 +277,12 @@ class Main extends BasicSHSign {
      * 单位合同的履行期间的承租人（主申请人）或同住人（配偶/子女）变更接口
      * 仅接受单位合同请求
      *
-     * @param string $pId           项目ID
-     * @param string $projectName   项目名称
-     * @param string $HID           房源ID
-     * @param string $contractNO    合同编号
-     * @param array  $oldTenantList 原承租人信息列表
-     * @param array  $newTenantList 新承租人信息列表
+     * @param string $pId 项目ID
+     * @param string $projectName 项目名称
+     * @param string $HID 房源ID
+     * @param string $contractNO 合同编号
+     * @param array $oldTenantList 原承租人信息列表
+     * @param array $newTenantList 新承租人信息列表
      *
      * @return mixed
      * @throws \ErrorException
@@ -303,16 +304,16 @@ class Main extends BasicSHSign {
             $new = $newTenantList;
         }
 
-        $data    = [
-            'companyId'         => $this->config['companyId'],
-            'companyName'       => $this->config['companyName'],
-            'PID'               => $pId,
-            'projectName'       => $projectName,
-            'HID'               => $HID,
-            'contractNO'        => $contractNO,
+        $data = [
+            'companyId' => $this->config['companyId'],
+            'companyName' => $this->config['companyName'],
+            'PID' => $pId,
+            'projectName' => $projectName,
+            'HID' => $HID,
+            'contractNO' => $contractNO,
             'contractAttribute' => '02',
-            'oldTenantList'     => $old,
-            'newTenantList'     => $new
+            'oldTenantList' => $old,
+            'newTenantList' => $new
         ];
         $headers = [
             'Content-Type' => 'application/json',
@@ -322,4 +323,23 @@ class Main extends BasicSHSign {
         return $this->httpPostJson($url, $data, $headers);
     }
 
+
+    /**
+     * 将数组中为null的值替换为空字符串
+     *
+     * @param $array array 要替换的数组
+     * @return void
+     * @author Free
+     */
+    private function convertNullToEmptyString(&$array) {
+        foreach ($array as &$value) {
+            if (is_array($value)) {
+                // 递归调用，处理子数组
+                $this->convertNullToEmptyString($value);
+            } elseif ($value === null) {
+                // 将值为null的字段转换为空字符串
+                $value = '';
+            }
+        }
+    }
 }
